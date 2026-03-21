46d2a878c97da26bddef055eff34623e3ab08540
James Tuazon <jtuazon7243@sdsu.edu>
2026-03-20 18:08:13 -0700

    Implemented ArrayStack<T> Devlog entry 1, session 1.

--- ArrayStack.h
+++ ArrayStack.h
@@ -12,25 +12,38 @@ private:
 public:
     void push(const T& value) {
         // TODO
+        data.push_back(value);
+        std::cout << "Pushed value " << value << std::endl;
     }
 
     void pop() {
         // TODO
+        if (empty()) {
+            std::cout << "Stack is empty, stack underflow." << std::endl;
+            return;
+        }
+        T val = data.back();
+        data.pop_back();
+        std::cout << "Popped " << val << std::endl;
     }
 
     T top() const {
         // TODO
-        return T();
+        if (empty()) {
+            std::cout << "Stack is empty" << std::endl;
+            return T();
+        }
+        return data.back();
     }
 
     bool empty() const {
         // TODO
-        return true;
+        return data.empty();
     }
 
     int size() const {
         // TODO
-        return 0;
+        return data.size();
     }
 };
 
---
+++ DEVLOG.md
@@ -0,0 +1,15 @@
+# Developer Log (DEVLOG.md)
+## Expression Engine
+
+---
+
+### Session 1
+**Date:** 2026-03-20
+**Task worked on:**
+Implemented ArrayStack<T>.
+**Bugs or Issues**
+No bugs or issues that I'm aware of.
+**Tests**
+Did not test implementation yet, that is for next session me. I am lazy right now.
+**Fixed or Improved**
+Improved ArrayStack<T>. It was blank before.
