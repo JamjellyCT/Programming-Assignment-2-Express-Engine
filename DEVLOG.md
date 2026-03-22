b5cae8acf3028819be588f83ee21255e0623b14a
James Tuazon <jtuazon7243@sdsu.edu>
2026-03-21 17:20:41 -0700

    Tested ArrayStack<T> implementation in main Devlog entry, session 2.

--- DEVLOG.md
+++ DEVLOG.md
@@ -13,3 +13,17 @@ No bugs or issues that I'm aware of.
 Did not test implementation yet, that is for next session me. I am lazy right now.
 **Fixed or Improved**
 Improved ArrayStack<T>. It was blank before.
+
+
+### Session 2
+**Date:** 2026-03-21
+**Task worked on:**
+Tested ArrayStack<T>.
+**Bugs or Issues**
+No bugs or issues that I'm aware of. Pop, push, and top, all worked as intended.
+**Tests**
+Tested pushing values int stack. Tested popping values from vector. Tested stack underflow edge case in pop.
+Tested finding top value of stack. Tested edge case of finding top of an empty stack.
+Tested if finding size and if stack empty works.
+**Fixed or Improved**
+Proved that ArrayStack implementation works as intended.
--- main.cpp
+++ main.cpp
@@ -64,29 +64,66 @@ double evalPostfix(const vector<Token>& tokens) {
 // Main
 
 int main() {
-    string line;
-    getline(cin, line);
+    // string line;
+    // getline(cin, line);
+    //
+    // vector<Token> tokens = tokenize(line);
+    //
+    // if (isValidPostfix(tokens)) {
+    //     cout << "FORMAT: POSTFIX\n";
+    //     cout << "RESULT: " << evalPostfix(tokens) << "\n";
+    // }
+    // else if (isValidInfix(tokens)) {
+    //     vector<Token> postfix = infixToPostfix(tokens);
+    //     cout << "FORMAT: INFIX\n";
+    //     cout << "POSTFIX: ";
+    //     for (const auto& t : postfix) {
+    //         cout << t.value << " ";
+    //     }
+    //     cout << "\n";
+    //     cout << "RESULT: " << evalPostfix(postfix) << "\n";
+    // }
+    // else {
+    //     cout << "FORMAT: NEITHER\n";
+    //     cout << "ERROR: invalid expression\n";
+    // }
 
-    vector<Token> tokens = tokenize(line);
 
-    if (isValidPostfix(tokens)) {
-        cout << "FORMAT: POSTFIX\n";
-        cout << "RESULT: " << evalPostfix(tokens) << "\n";
-    }
-    else if (isValidInfix(tokens)) {
-        vector<Token> postfix = infixToPostfix(tokens);
-        cout << "FORMAT: INFIX\n";
-        cout << "POSTFIX: ";
-        for (const auto& t : postfix) {
-            cout << t.value << " ";
-        }
-        cout << "\n";
-        cout << "RESULT: " << evalPostfix(postfix) << "\n";
-    }
-    else {
-        cout << "FORMAT: NEITHER\n";
-        cout << "ERROR: invalid expression\n";
-    }
+    //Manual Test
+    ArrayStack<int> myStack;
+    cout << "Is empty: " << myStack.empty() << endl;
+    cout << "Top element: " << myStack.top() << endl;
+    myStack.pop();
+
+    cout << endl;
+
+    myStack.push(10);
+    myStack.push(20);
+    cout << "Top element: " << myStack.top() << endl;
+    myStack.push(30);
+    cout << "Top element: " << myStack.top() << endl;
+    cout << "Is empty: " << myStack.empty() << endl;
+    cout << "Size: " << myStack.size() << endl;
+
+    cout << endl;
+
+    myStack.pop();
+    myStack.pop();
+
+    cout << "Top element: " << myStack.top() << endl;
+
+    myStack.pop();
+    myStack.pop();
+
+    cout << "Top element: " << myStack.top() << endl;
+    cout << "Is empty: " << myStack.empty() << endl;
+    cout << "Size: " << myStack.size() << endl;
+
+    cout << endl;
+
+    myStack.push(10);
+    myStack.push(20);
+
 
     return 0;
 }
